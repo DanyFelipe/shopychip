fcaebc6ff41340e089099e46098531e28a8f873e
DanyFelipe <danyfelipe117@gmail.com>
2025-09-18 10:36:49 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/com/daniel/shopychip/ShopychipApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.shopychip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopychipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopychipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/shopychip/ShopychipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.shopychip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopychipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
